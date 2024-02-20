0794c483d355981665a07b850f260f7ab8d31bdd
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-02-19 21:15:27 -0300

    Add FluentValidation cliente

---
+++ ambclientes/Application/Validations/ClienteValidator.cs
@@ -0,0 +1,34 @@
+﻿using Amb.Clientes.Domain.Models;
+using FluentValidation;
+
+namespace Amb.Clientes.Application.Validations;
+
+public class ClienteValidator: AbstractValidator<ClienteDto>
+{
+    public ClienteValidator()
+    {
+        
+        RuleFor(x => x.Nombre).NotNull().NotEmpty().MaximumLength(50);
+        
+        RuleFor(x => x.Apellido).NotNull().NotEmpty().MaximumLength(50);
+        
+        RuleFor(x => x.Cuit)
+            .NotNull()
+            .MinimumLength(10)
+            .MaximumLength(11);
+        
+        RuleFor(x => x.FechaNacimiento)
+            .Configure(x => x.SetDisplayName("Fecha de nacimiento"));
+        
+        RuleFor(x => x.Mail)
+            .NotNull()
+            .NotEmpty()
+            .EmailAddress();
+        
+        RuleFor(x => x.Telefono)
+            .NotNull()
+            .NotEmpty()
+            .Must(x => x.ToString().StartsWith("11") && x.ToString().Length == 10)
+            .WithMessage("Número de celular debe comenzar con 11");
+    }
+}
