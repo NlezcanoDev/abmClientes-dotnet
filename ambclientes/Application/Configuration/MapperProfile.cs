8a38b3c8b869fe4ba4fe725472670eb6b4aed923
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-02-19 21:15:13 -0300

    Add MapperProfile.cs

---
+++ ambclientes/Application/Configuration/MapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using Amb.Clientes.Domain.Entities;
+using Amb.Clientes.Domain.Models;
+using AutoMapper;
+
+namespace Amb.Clientes.Application.Configuration;
+
+public class MapperProfile: Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<Cliente, ClienteDto>().ReverseMap();
+    }
+}
