c7acefa7f2407c5566bab33d40ca1f98ee83a42c
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-02-19 21:15:59 -0300

    Add controllers - Middlewares - config Program.cs

---
+++ ambclientes/Controllers/ClienteController.cs
@@ -0,0 +1,56 @@
+﻿using Amb.Clientes.Application.Admin;
+using Amb.Clientes.Domain.Filters;
+using Amb.Clientes.Domain.Models;
+using Amb.Clientes.Persistence.Repository;
+
+namespace Amb.Clientes.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/clientes")]
+[ApiController]
+public class ClienteController: ControllerBase
+{
+    private ClienteAdmin _admin;
+
+    public ClienteController(IClienteRepository repository)
+    {
+        _admin = new ClienteAdmin
+        {
+            Repository = repository
+        };
+    }
+    
+    [HttpGet]
+    public IActionResult Get([FromQuery]BaseFilter filter)
+    {
+        var data = _admin.Get(filter);
+        return Ok(data);
+    }
+    
+    [HttpGet("all")]
+    public IActionResult GetAll(){
+        var data = _admin.GetAll();
+        return Ok(data);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var data = await _admin.GetById(id);
+        return Ok(data);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] ClienteDto dto)
+    {
+        var data = await _admin.Create(dto);
+        return Ok(data);    
+    }
+    
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, [FromBody] ClienteDto dto)
+    {
+        var data = await _admin.Update(id, dto);
+        return Ok(data);
+    }
+}
---
+++ ambclientes/DependencyInjectionService.cs
@@ -0,0 +1,34 @@
+﻿using Amb.Clientes.Application.Admin;
+using Amb.Clientes.Application.Configuration;
+using Amb.Clientes.Persistence.Database;
+using Amb.Clientes.Persistence.Repository;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Amb.Clientes;
+
+public static class DependencyInjectionService
+{
+    public static IServiceCollection AddServiceCollection(this IServiceCollection services, IConfiguration configuration)
+    {
+        #region Application
+        var mapper = new MapperConfiguration(config =>
+        {
+            config.AddProfile(new MapperProfile());
+        });
+        services.AddSingleton(mapper.CreateMapper());
+
+        #endregion
+
+        #region Persistence
+        services.AddDbContext<DatabaseService>(opt =>
+            opt.UseSqlServer(configuration["SQLConnectionString"]));
+
+        services.AddScoped<DatabaseService>();
+
+        services.AddScoped<IClienteRepository, ClienteRepository>();
+        #endregion
+        
+        return services;
+    }
+}
